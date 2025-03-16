98b26ab888c1756ca43f69d48cc6cf2d1c913d56
David Petkov <davidpetkov2006@gmail.com>
2025-03-16 15:38:01 +0200

    asd

--- src/components/FlameButton.tsx
+++ src/components/FlameButton.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const FlameButton: React.FC = () => {
   const handleClick = () => {
-    window.location.href = "https://dashboard.hoteach.com";
+    window.location.href = "https://app.hoteach.com";
   };
 
   return (
